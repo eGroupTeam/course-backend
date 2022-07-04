117ad84c560406502c1c83c822f51d65141fb387
Ben Wu <jitsung.wu@gmail.com>
2022-07-04 15:05:50 +0800

    course04

---
+++ course04/demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class TestController{
+    @GetMapping("/test")
+    @ResponseBody
+    public String index() {
+        return "<h1>Hi! This is Test!</h1>";
+    }
+
+    @GetMapping("/customer")
+    public String getCustomer() {
+        return "customer.html";
+    }
+
+    @GetMapping("/test/request")
+    @ResponseBody
+    public String request(@ModelAttribute("id") String name){
+       
+       return name;
+    }
+}
