3e7d6b0a7fe0685ee966bd6642fe9af52a451145
Ben Wu <jitsung.wu@gmail.com>
2022-07-05 12:03:04 +0800

    Course05

---
+++ course05/demo/src/main/java/com/example/demo/entity/Customer.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+public class Customer {
+  private String name;
+  public void setName(String name){
+    this.name = name;
+  }
+  public String getName(){
+    return name;
+  }  
+}
