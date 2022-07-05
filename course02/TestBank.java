a0ad819c2c5ce8568321032974e2d22fc1eeb4da
shiunchiu <egroup.intern.shiunchiu@gmail.com>
2022-07-05 23:52:00 +0800

    fix: debug homework

--- course02/TestBank.java
+++ course02/TestBank.java
@@ -1,20 +1,28 @@
 public class TestBank {
     public static void main(String[] args) {
-        // Bank myBank = new Bank(2);
-        // myBank.addAccount(10000);
-        // myBank.addAccount(20000);
-        // myBank.addAccount(30000);//應該不會被加進去
-        // myBank.displayAllAccounts();
-        // myBank.calculateInterest();
-        // myBank.displayAllAccounts();
+
+        Bank myBank = new Bank(2);
+        myBank.addAccount(10000);
+        myBank.addAccount(20000);
+        myBank.addAccount(30000);//應該不會被加進去
+        System.out.println("myBank: ");
+        myBank.displayAllAccounts();
+        myBank.calculateInterest();
+        System.out.println("After calculateInterest");
+        myBank.displayAllAccounts();
+
+        System.out.println();
 
         Bank shiunBank = new Bank(2);
         shiunBank.addAccount(66666);
         shiunBank.addAccount(123456);
         shiunBank.addAccount(54321);
+        System.out.println("shiunBank: ");
         shiunBank.displayAllAccounts();
         shiunBank.calculateInterest();
         System.out.println("After calculateInterest");
         shiunBank.displayAllAccounts();
+
+        
     }
 }
