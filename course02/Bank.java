9d61b9ea19e9fff598683a00e68cc2058594b595
shiunchiu <egroup.intern.shiunchiu@gmail.com>
2022-07-06 01:03:18 +0800

    feat: add array version

--- course02/Bank.java
+++ course02/Bank.java
@@ -2,12 +2,15 @@ public class Bank {
     private Account allAccounts[];
     private int count=0;//目前帳戶數
     private int maxAccount;//最多只能有這麼多帳戶
+    private String bankname;
     private static double interestRate = .006;
 
-    public Bank(int maxAccount){
+    public Bank(int maxAccount, String bankname) {
         this.maxAccount = maxAccount;
+        this.bankname = bankname;
         allAccounts = new Account[maxAccount];
     }
+    
 
     public void addAccount(long balance){
         if (count < maxAccount){
@@ -29,16 +32,16 @@ public class Bank {
 
     public void displayAllAccounts(){
         for (int i=0; i<count; i++){
-            System.out.println("Account " + i + ": " + allAccounts[i].getBalance());
+            System.out.println(bankname + ", Account " + i + ": " + allAccounts[i].getBalance());
         }
 
     }
     public static void main(String[] args) {
-        Bank myBank = new Bank(2);
-        myBank.addAccount(100);
-        myBank.addAccount(200);
-        myBank.addAccount(300);//應該不會被加進去
-        myBank.displayAllAccounts();
+        // Bank myBank = new Bank(2, "mybank");
+        // myBank.addAccount(100);
+        // myBank.addAccount(200);
+        // myBank.addAccount(300);//應該不會被加進去
+        // myBank.displayAllAccounts();
 
 
     }
--- course02/TestBank.java
+++ course02/TestBank.java
@@ -1,27 +1,47 @@
 public class TestBank {
     public static void main(String[] args) {
+        Bank[] allBank = new Bank[2];
+        allBank[0] = new Bank(2, "mybank");
+        allBank[1] = new Bank(3, "shiunbank");
 
-        Bank myBank = new Bank(2);
-        myBank.addAccount(10000);
-        myBank.addAccount(20000);
-        myBank.addAccount(30000);//應該不會被加進去
-        System.out.println("myBank: ");
-        myBank.displayAllAccounts();
-        myBank.calculateInterest();
-        System.out.println("After calculateInterest");
-        myBank.displayAllAccounts();
+        allBank[0].addAccount(2000);
+        allBank[0].addAccount(3000);
 
-        System.out.println();
+        allBank[1].addAccount(5000);
+        allBank[1].addAccount(6000);
+        allBank[1].addAccount(7000);
 
-        Bank shiunBank = new Bank(2);
-        shiunBank.addAccount(66666);
-        shiunBank.addAccount(123456);
-        shiunBank.addAccount(54321);
-        System.out.println("shiunBank: ");
-        shiunBank.displayAllAccounts();
-        shiunBank.calculateInterest();
-        System.out.println("After calculateInterest");
-        shiunBank.displayAllAccounts();
+        for(int i = 0; i < allBank.length; i++){
+            allBank[i].displayAllAccounts();
+            allBank[i].calculateInterest();
+        }
+        System.out.println("\n=========After calculateInterest=========\n");
+        for(int i = 0; i < allBank.length; i++){
+            allBank[i].displayAllAccounts();
+        }
+
+        // Bank myBank = new Bank(2);
+        // myBank.addAccount(10000);
+        // myBank.addAccount(20000);
+        // myBank.addAccount(30000);//應該不會被加進去
+        // System.out.println("myBank: ");
+        // myBank.displayAllAccounts();
+        // myBank.calculateInterest();
+        // System.out.println("After calculateInterest");
+        // myBank.displayAllAccounts();
+
+        // System.out.println();
+
+        // Bank shiunBank = new Bank(2);
+        // shiunBank.addAccount(66666);
+        // shiunBank.addAccount(123456);
+        // shiunBank.addAccount(54321);
+        // System.out.println("shiunBank: ");
+        // shiunBank.displayAllAccounts();
+        // shiunBank.calculateInterest();
+        // System.out.println("After calculateInterest");
+        // shiunBank.displayAllAccounts();
+        
 
         
     }
