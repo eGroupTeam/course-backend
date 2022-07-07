629f3d97de42ed3a2c61f4bc8ac7c1b5f511c202
dihungyu <egroup.intern.dihungyu@gmail.com>
2022-07-07 21:14:27 +0800

    homework5

--- course05/demo/src/main/java/com/example/demo/controller/MainController.java
+++ course05/demo/src/main/java/com/example/demo/controller/MainController.java
@@ -1,30 +1,25 @@
 package com.example.demo.controller;
 
 import javax.servlet.http.HttpServletRequest;
-import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.entity.Customer;
-  @Controller
+  @RestController
   public class MainController{
     @GetMapping({"/","/hello"})
-    @ResponseBody
     public String index() {
         return "<h1>Hi!</h1>";
     }
     @GetMapping("/request")
-    @ResponseBody
     public String request(HttpServletRequest request) {
       //System.out.println("get");
       String name = request.getParameter("id");
       return name;
     }
     @PostMapping("/request")
-    @ResponseBody
     public String postRequest(@ModelAttribute Customer customer) {
       //System.out.println("post");
       return customer.getName();
