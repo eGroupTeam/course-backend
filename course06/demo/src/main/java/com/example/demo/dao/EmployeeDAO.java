65f17631a1447add643377cba382b7e7d54e0175
dihungyu <egroup.intern.dihungyu@gmail.com>
2022-07-07 23:55:10 +0800

    homework6

---
+++ course06/demo/src/main/java/com/example/demo/dao/EmployeeDAO.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import com.example.demo.entity.Employee;
+@Repository
+
+public interface EmployeeDAO {
+    public List<Employee> findAll();
+
+  public Employee findOne(Long id);
+}
--- course06/demo/src/main/java/com/example/demo/entity/Employee.java
+++ course06/demo/src/main/java/com/example/demo/entity/Employee.java
@@ -1,11 +1,17 @@
 package com.example.demo.entity;
 public class Employee{
+    private Long id;
     private String name;
+    private String gender;
     private String department;
 
-    public Employee(String name, String department ) {
+    public Employee(Long id, String name, String gender, String department ) {
       this.name = name;
       this.department = department;
+      this.gender = gender;
+    }
+
+    public Employee(String string, String string2, String string3) {
     }
 
     public String getName() {
@@ -22,5 +28,21 @@ public class Employee{
 
     public void setDepartment(String department) {
       this.department = department;
+    }
+
+    public String getGender() {
+      return gender;
+    }
+
+    public void setGender(String gender) {
+      this.gender = gender;
+    }
+
+    public Long getId() {
+      return id;
+    }
+
+    public void setId(Long id) {
+      this.id = id;
     }
 }
