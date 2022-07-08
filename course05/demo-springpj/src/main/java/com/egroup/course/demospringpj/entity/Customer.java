83fe3e5123a75d450269f9545eadb6329e1424f3
kevinlin3 <egroup.intern.kevinlin@gmail.com>
2022-07-09 02:50:41 +0800

    hw05 done

---
+++ course05/demo-springpj/src/main/java/com/egroup/course/demospringpj/entity/Customer.java
@@ -0,0 +1,27 @@
+package com.egroup.course.demospringpj.entity;
+
+public class Customer {
+  private String name;
+  private int age;
+
+  public Customer(String name, int age){
+    this.name=name;
+    this.age=age;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+}
---
+++ course05/demo-springpj/src/test/java/com/egroup/course/demospringpj/DemoSpringpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egroup.course.demospringpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringpjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
