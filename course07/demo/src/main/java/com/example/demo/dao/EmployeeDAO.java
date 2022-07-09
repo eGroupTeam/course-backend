4faa597e3685543e22a87e3bc8fd2bb0d8ce1d86
dihungyu <egroup.intern.dihungyu@gmail.com>
2022-07-09 16:50:22 +0800

    homework7

---
+++ course07/demo/src/main/java/com/example/demo/dao/EmployeeDAO.java
@@ -0,0 +1,12 @@
+package com.example.demo.dao;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import com.example.demo.entity.Employee;
+@Repository
+public interface EmployeeDAO {
+  public List<Employee> findAll();
+  public Employee findOne(Long id) throws Exception;
+  public int createEmployee(Employee employee);
+  public int updateEmployee(Employee employee);
+  public int deleteEmployee(int id);
+}
