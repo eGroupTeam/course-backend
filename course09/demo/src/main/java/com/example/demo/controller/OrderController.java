3397b5e2b3a5db0ab18957cdc84c11e0fee7c43a
ryanbohongcho <egroup.intern.ryancho@gmail.com>
2022-07-11 22:31:54 +0800

    course09 done

--- course09/demo/src/main/java/com/example/demo/controller/OrderController.java
+++ course09/demo/src/main/java/com/example/demo/controller/OrderController.java
@@ -1,15 +1,20 @@
 package com.example.demo.controller;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.demo.dao.SalesOrderDAO;
 import com.example.demo.entity.ProductSales;
+import com.example.demo.entity.ProductTime;
 import com.example.demo.entity.SalesOrder;
 
 @RestController
@@ -25,6 +30,16 @@ public class OrderController {
   @GetMapping("/order")
   public List<ProductSales> getList() throws Exception {
     return dao.getList();
+  }
+
+  @GetMapping(value = {"/order/time/{time1}/{time2}"})
+  public List<ProductTime> getSalesBetweenList(@PathVariable("time1") String time1,@PathVariable("time2") String time2) throws SQLException, Exception{
+    List<ProductTime> product = dao.getSalesBetweenList(time1, time2);
+    if (product.size() == 0){
+      throw new ResponseStatusException(
+        HttpStatus.NOT_FOUND, "無資料");
+    }
+    return product;
   }
 
 }
--- course09/demo/src/main/java/com/example/demo/dao/SalesOrderDAO.java
+++ course09/demo/src/main/java/com/example/demo/dao/SalesOrderDAO.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.ProductSales;
+import com.example.demo.entity.ProductTime;
 import com.example.demo.entity.SalesOrder;
 
 @Repository
 public interface SalesOrderDAO {
   public int insert(SalesOrder order) throws Exception;
-
   public List<ProductSales> getList() throws Exception;
+  public List<ProductTime> getSalesBetweenList(String time1, String time2) throws Exception;
 }
