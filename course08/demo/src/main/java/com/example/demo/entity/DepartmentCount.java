e1448f95d3efa9f2296949124f86db51f25fe671
kevinlin3 <egroup.intern.kevinlin@gmail.com>
2022-07-12 05:25:35 +0800

    hw08

--- course08/demo/src/main/java/com/example/demo/entity/DepartmentCount.java
+++ course08/demo/src/main/java/com/example/demo/entity/DepartmentCount.java
@@ -4,6 +4,9 @@ public class DepartmentCount {
     private String department;
     private int count;
 
+    public DepartmentCount() {
+    }
+
     public DepartmentCount(String department, int count) {
         this.department = department;
         this.count = count;
@@ -23,6 +26,40 @@ public class DepartmentCount {
 
     public void setCount(int count) {
         this.count = count;
+    }
+
+    public DepartmentCount department(String department) {
+        setDepartment(department);
+        return this;
+    }
+
+    public DepartmentCount count(int count) {
+        setCount(count);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof DepartmentCount)) {
+            return false;
+        }
+        DepartmentCount departmentCount = (DepartmentCount) o;
+        return Objects.equals(department, departmentCount.department) && count == departmentCount.count;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(department, count);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " department='" + getDepartment() + "'" +
+            ", count='" + getCount() + "'" +
+            "}";
     }
     
 }
