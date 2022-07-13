6017586cd13e10a61c25d14f0891580848b34381
ginnypinjyunlin <egroup.intern.ginnylin@gmail.com>
2022-07-13 23:56:57 +0800

    (屍體 )SQL學習中

--- course08/demo/src/main/java/com/example/demo/controller/EmployeeController.java
+++ course08/demo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,17 +1,22 @@
 package com.example.demo.controller;
-import java.util.ArrayList;
-
+//import java.util.ArrayList;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.server.ResponseStatusException;
+import com.example.demo.entity.Employee;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
-import com.example.demo.entity.Employee;
+import com.example.demo.dao.EmployeeDAO;
+import com.example.demo.entity.DepartmentCount;
 
 @RestController
 public class EmployeeController {
