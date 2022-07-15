50b41b9686bebf4cd70afaddfee7f1bb24ee3ce2
Ben Wu <jitsung.wu@gmail.com>
2022-07-15 17:52:23 +0800

    course 12 & 13 done

--- course13/README.md
+++ course13/README.md
@@ -1,1 +1,25 @@
-# frontend
+# course-backend
+
+## 第十三章 前後端整合 2
+
+### 修改
+
+    @PutMapping(value = "/product/{id}")
+    public void processFormUpdate(@RequestBody Product product, @PathVariable("id") int id) throws Exception {
+      if (product.getId() == 0) {// if not set, getId() will return 0
+        product.setId(id);
+      }
+
+      int result = dao.update(product);
+      if (result == 0) {
+        throw new SQLException("id: " + id + " 並不存在");
+      }
+    }
+
+並且產生對應的 DAO 及 implementation，請參考 CustomerDAO.java 及 CustomerDAOImpl.java
+
+    public int update(Product product) throws Exception;
+
+接下來透過 postman 先測試一下。如果正確，我們就可以修改一下 react 的對應程式。
+
+### 作業 我們根據 product 來完成 customer 的 CRUD
---
+++ course13/demo/src/main/java/com/example/demo/entity/SalesOrderItem.java
@@ -0,0 +1,58 @@
+package com.example.demo.entity;
+
+public class SalesOrderItem {
+  private int id;
+  private int orderId;
+  private int productId;
+  private int amount;
+  private int price;
+
+  public SalesOrderItem(int id, int orderId, int productId, int amount, int price) {
+    this.id = id;
+    this.orderId = orderId;
+    this.productId = productId;
+    this.amount = amount;
+    this.price = price;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getOrderId() {
+    return this.orderId;
+  }
+
+  public void setOrderId(int orderId) {
+    this.orderId = orderId;
+  }
+
+  public int getProductId() {
+    return this.productId;
+  }
+
+  public void setProductId(int productId) {
+    this.productId = productId;
+  }
+
+  public int getAmount() {
+    return this.amount;
+  }
+
+  public void setAmount(int amount) {
+    this.amount = amount;
+  }
+
+  public int getPrice() {
+    return this.price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+}
