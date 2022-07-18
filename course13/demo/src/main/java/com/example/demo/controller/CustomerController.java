3171757de617a6ac3c8a679d5afe45d6fb674a98
ryanbohongcho <egroup.intern.ryancho@gmail.com>
2022-07-18 23:13:17 +0800

    course13 done

--- course13/demo/src/main/java/com/example/demo/controller/CustomerController.java
+++ course13/demo/src/main/java/com/example/demo/controller/CustomerController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,7 @@ import com.example.demo.dao.CustomerDAO;
 import com.example.demo.entity.AddressCount;
 import com.example.demo.entity.Customer;
 @RestController
+@CrossOrigin
 public class CustomerController {
   @Autowired
   CustomerDAO dao;
@@ -107,6 +110,15 @@ public class CustomerController {
     int result = dao.update(customer);
     if (result == 0){
       throw new SQLException("id: "+id+" 並不存在");
+    }
+  }
+
+  @DeleteMapping(value = "/customer/{id}")
+  public void delete(@PathVariable("id") Long id) throws Exception {
+
+    int result = dao.delete(id);
+    if (result == 0) {
+      throw new SQLException("id: " + id + " 並不存在");
     }
   }
 }
