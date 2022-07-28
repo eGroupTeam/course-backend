63288c9daeab82334f35cfc8e37041d0581b7ecc
kevinlin3 <egroup.intern.kevinlin@gmail.com>
2022-07-29 07:43:32 +0800

    微調

--- 202207 Test/src/main/java/egroup/test202207/controller/OrgnztnController.java
+++ 202207 Test/src/main/java/egroup/test202207/controller/OrgnztnController.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import egroup.test202207.dao.OrganizationDAO;
 import egroup.test202207.entity.Organization;
 
+@CrossOrigin
 @RestController
 public class OrgnztnController {
 
--- 202207 Test/src/main/java/egroup/test202207/controller/ProductController.java
+++ 202207 Test/src/main/java/egroup/test202207/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import egroup.test202207.dao.ProductDAO;
 import egroup.test202207.entity.Product;
 
+@CrossOrigin
 @RestController
 public class ProductController {
     @Autowired
