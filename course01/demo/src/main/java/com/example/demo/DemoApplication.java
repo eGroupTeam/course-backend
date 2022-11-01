1d69c20d248251d35d2484054adb84953c94ca59
chennyday <daychenny@gmail.com>
2022-11-01 18:09:08 +0800

    feat: spring demo

--- course01/demo/src/main/java/com/example/demo/DemoApplication.java
+++ course01/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -13,9 +13,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping("/hello")
-		public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Welcome to Ben's App! Hello %s!", name);
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "world") String name){
+		return String.format("Hello %s!", name);
 	}
 
 }
