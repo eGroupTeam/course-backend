9a9744a4510e9951a0bb1efb8f2be9cd09e9a4ec
cyyad <egroup.intern.dayyc@gmail.com>
2022-11-07 22:32:15 +0800

    feat: hw6

--- course06/demo/src/main/java/com/example/demo/entity/Employee.java
+++ course06/demo/src/main/java/com/example/demo/entity/Employee.java
@@ -1,9 +1,11 @@
 package com.example.demo.entity;
 public class Employee{
+    private Long id;
     private String name;
     private String department;
 
-    public Employee(String name, String department ) {
+    public Employee(Long id, String name, String department ) {
+      this.id = id;
       this.name = name;
       this.department = department;
     }
@@ -15,12 +17,20 @@ public class Employee{
     public void setName(String name) {
       this.name = name;
     }
-
+    
     public String getDepartment() {
       return department;
     }
 
     public void setDepartment(String department) {
       this.department = department;
+    }
+
+    public Long getId(){
+      return id;
+    }
+
+    public void setId(Long id){
+      this.id = id;
     }
 }
