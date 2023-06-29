e538548cbd4ca5906a81361d97a4d043ae2b4405
jimmy614 <egroup.intern.jimmyhsu@gmail.com>
2023-06-29 16:16:25 +0800

    feat: hw

--- course02/TestBank.java
+++ course02/TestBank.java
@@ -1,12 +1,26 @@
 public class TestBank {
     public static void main(String[] args) {
-        Bank myBank = new Bank(2);
-        myBank.addAccount(10000);
-        myBank.addAccount(20000);
-        myBank.addAccount(30000);//應該不會被加進去
-        myBank.displayAllAccounts();
-        myBank.calculateInterest();
-        myBank.displayAllAccounts();
+        // Bank myBank = new Bank(2);
+        // myBank.addAccount(10000);
+        // myBank.addAccount(20000);
+        // myBank.addAccount(30000);//應該不會被加進去
+        // myBank.displayAllAccounts();
+        // myBank.calculateInterest();
+        // myBank.displayAllAccounts();
+
+        Bank myBank1Bank = new Bank(3);
+        myBank1Bank.addAccount(20000);
+        myBank1Bank.addAccount(30000);
+        myBank1Bank.addAccount(40000);//應該不會被加進去
+        System.out.println("**************");
+        myBank1Bank.displayAllAccounts();
+        System.out.println("**************");
+        myBank1Bank.calculateInterest();
+        System.out.println("**************");
+        myBank1Bank.displayAllAccounts();
+        System.out.println("**************");
+
+
 
     }
 }
