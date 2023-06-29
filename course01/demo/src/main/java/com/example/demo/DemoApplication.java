62d9b1eec2ae5166ae8a2a917a9e60adc325fcb6
jimmy614 <egroup.intern.jimmyhsu@gmail.com>
2023-06-29 14:07:15 +0800

    feat: homework1

--- course01/demo/src/main/java/com/example/demo/DemoApplication.java
+++ course01/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
@@ -12,10 +9,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-	@GetMapping("/hello")
-		public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Welcome to Ben's App! Hello %s!", name);
 	}
 
 }
