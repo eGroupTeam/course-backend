2bb553c297be2abf355921f14d9abac82daa311f
jimmy614 <egroup.intern.jimmyhsu@gmail.com>
2023-06-30 13:11:30 +0800

    feat: hw

--- course05/demo/src/main/java/com/example/demo/entity/Customer.java
+++ course05/demo/src/main/java/com/example/demo/entity/Customer.java
@@ -2,10 +2,22 @@ package com.example.demo.entity;
 
 public class Customer {
   private String name;
+  private int age;
+  
+  public Customer(String name, int age){
+    this.name = name;
+    this.age = age;
+  }
   public void setName(String name){
     this.name = name;
   }
   public String getName(){
     return name;
   }  
+  public void setAge(int age){
+    this.age = age;
+  }
+  public int getAge(){
+    return age;
+  }
 }
